f5f212433d6de833f7f2e9e2d88391ed8ba0faeb
MuhammadAswinIbrahim <aswinibrahim4@gmail.com>
2023-03-31 12:11:09 +0800

    gak pake s

--- app/Models/customer.php
+++ app/Models/customer.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
-    protected $table='customers';
+    protected $table='customer';
     protected $fillable = [
         'nama',
         'tanggal_lahir',
